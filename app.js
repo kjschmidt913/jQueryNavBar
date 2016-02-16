74314ef9fc62b7440adf0ebe8ab93a58abac0ff4
KJ <kj.schmidt913@gmail.com>
2016-02-16 15:00:00 -0500

    Nav bar, without visited links holind the formatting

---
+++ app.js
@@ -0,0 +1,20 @@
+/*  jQuery ready function. Then, specify a function to execute when the DOM is fully loaded.  */
+$(document).ready(
+  /* This is the function that will get executed after the DOM is fully loaded */
+  function () {
+    /* Next part handles hovering effect and submenu appearing */
+    $('li').mouseenter(function () { //fade on hover
+        $(this).fadeTo('fast', 0.25);
+      });
+    $('li').mouseleave(function () { //back to normal 
+        $(this).fadeTo('fast', 1);
+      });
+    $('li').click(function () { /*background change when clicked*/
+        $(this).css('background-color', 'purple');
+
+      });
+    $('li').click(function(){ //says "clicked!" when clicked
+        $(this).text("Clicked!");
+      });
+
+});
